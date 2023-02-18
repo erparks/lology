c08b0d28c334328a435a08cc74e3f765f2a7f1f9
Ethan Parks <erparks@gmail.com>
2023-02-18 06:56:05 -0700

    make config file if none exists

--- cmd/keyset/main.go
+++ cmd/keyset/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/spf13/viper"
 )
 
 func main() {
+	fileName := "client"
+	extension := "json"
+	fullname := fileName + "." + extension
+
+	if _, err := os.Stat(fullname); err != nil {
+		if f, err := os.Create(fullname); err != nil {
+			log.Fatal(err)
+		} else {
+			f.Close()
+		}
+	}
+
 	viper.SetConfigName("client")
 	viper.SetConfigType("json")
 	viper.AddConfigPath(".")
